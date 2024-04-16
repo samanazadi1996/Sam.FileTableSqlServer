6cb0f69b9aac9675ab45b19eb59fead4a1a55864
saman azadi <samanazadi1996@gmail.com>
2024-04-17 01:19:30 +0330

    update SqlQueriesExtention

--- Src/Sam.FileTableFramework/Extentions/Utilities/SqlQueriesExtention.cs
+++ Src/Sam.FileTableFramework/Extentions/Utilities/SqlQueriesExtention.cs
@@ -1,42 +1,38 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Xml.Linq;
-
-namespace Sam.FileTableFramework.Extentions.Utilities
+﻿namespace Sam.FileTableFramework.Extentions.Utilities
 {
     internal class SqlQueriesExtention
     {
         internal class MigrationQueries
         {
             public static string CountOfDatabase(string name)
-                => string.Format("SELECT COUNT(*) FROM SYS.DATABASES WHERE [name]='{0}'", name);
+                => $"SELECT COUNT(*) FROM SYS.DATABASES WHERE [name]='{name}'";
             public static string DirectoryOfDatabases()
                 => "SELECT SERVERPROPERTY('INSTANCEDEFAULTDATAPATH')";
             public static string CountOfTable(string name)
-                => string.Format("SELECT COUNT(*) FROM SYS.TABLES WHERE [name] = '{0}' AND [is_filetable] = 1", name);
+                => $"SELECT COUNT(*) FROM SYS.TABLES WHERE [name] = '{name}' AND [is_filetable] = 1";
             public static string CreateTable(string name)
-                => string.Format("CREATE TABLE [{0}] AS FILETABLE", name);
+                => $"CREATE TABLE [{name}] AS FILETABLE";
             public static string CreateDatabase(string name, string path)
-                => string.Format("CREATE DATABASE {0} ON PRIMARY (NAME=F1,FILENAME='{1}{0}.MDF'),FILEGROUP G1 CONTAINS FILESTREAM(NAME=Str,FILENAME='{1}{0}') LOG ON (NAME=F2,FILENAME='{1}{0}Log.MDF') WITH FILESTREAM (NON_TRANSACTED_ACCESS=FULL,DIRECTORY_NAME=N'{0}')", name, path);
+                =>$@"CREATE DATABASE {name} ON PRIMARY (NAME=f1, filename='{path}{name}.MDF'),
+filegroup g1 CONTAINS filestream(NAME=str, filename='{path}{name}') log ON (NAME
+=f2, filename='{path}{name}Log.MDF') WITH filestream (non_transacted_access=FULL
+, directory_name=N'{name}') ";
 
         }
         internal class RepositoryQueries
         {
             public static string Find(string tableName, string fileName)
-                => string.Format("SELECT TOP 1 * FROM [{0}] WHERE [name] = '{1}'", tableName, fileName);
+                => $"SELECT TOP 1 * FROM [{tableName}] WHERE [name] = '{fileName}'";
             public static string Insert(string tableName, string fileName, string dParams)
-                => string.Format("INSERT INTO {0} ([name],[file_stream]) VALUES ('{1}',{2})", tableName, fileName, dParams);
+                => $"INSERT INTO {tableName} ([name],[file_stream]) VALUES ('{fileName}',{dParams})";
             public static string SelectPaging(string tableName, int skip, int take)
-                => string.Format("SELECT *  FROM [{0}] ORDER BY name OFFSET {1} ROWS FETCH NEXT {2} ROWS ONLY", tableName, skip, take);
+                => $"SELECT *  FROM [{tableName}] ORDER BY name OFFSET {skip} ROWS FETCH NEXT {take} ROWS ONLY";
             public static string SelectAll(string tableName)
-                => string.Format("SELECT * FROM [{0}]", tableName);
+                => $"SELECT * FROM [{tableName}]";
             public static string Count(string tableName)
-                => string.Format("SELECT COUNT(*) FROM [{0}]", tableName);
+                => $"SELECT COUNT(*) FROM [{tableName}]";
             public static string Delete(string tableName, string fileName)
-                => string.Format("DELETE [{0}] WHERE [name] = '{1}'", tableName, fileName);
+                => $"DELETE [{tableName}] WHERE [name] = '{fileName}'";
         }
     }
 }
