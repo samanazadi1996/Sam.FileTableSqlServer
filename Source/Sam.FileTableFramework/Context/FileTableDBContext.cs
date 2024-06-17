f438fa2b0c574a3504ddeede73a67671768fe08f
saman azadi <samanazadi1996@gmail.com>
2024-06-17 14:06:31 +0330

    scoped

--- Source/Sam.FileTableFramework/Context/FileTableDBContext.cs
+++ Source/Sam.FileTableFramework/Context/FileTableDBContext.cs
@@ -1,7 +1,28 @@
-﻿namespace Sam.FileTableFramework.Context
+﻿using Sam.FileTableFramework.Extentions;
+using System;
+using System.Linq;
+
+namespace Sam.FileTableFramework.Context
 {
     public abstract class FileTableDBContext
     {
-        internal string? ConnectionString { get; set; }
+        internal DatabaseOptions options { get; set; }
+        public FileTableDBContext(DatabaseOptions options)
+        {
+            this.options = options;
+
+            var props = GetType().GetProperties().Where(p => typeof(FtDbSet).IsAssignableFrom(p.PropertyType));
+
+            foreach (var item in props)
+            {
+                var ftDbSetInstance = Activator.CreateInstance(item.PropertyType);
+
+                typeof(FtDbSet).GetProperty("TableName").SetValue(ftDbSetInstance, item.Name);
+                typeof(FtDbSet).GetProperty("ConnectionString").SetValue(ftDbSetInstance, options.ConnectionString);
+
+                GetType().GetProperty(item.Name).SetValue(this, ftDbSetInstance);
+            }
+
+        }
     }
 }
--- Source/Sam.Persistence/DatabaseContext.cs
+++ Source/Sam.Persistence/DatabaseContext.cs
@@ -1,9 +1,14 @@
 ﻿using Sam.FileTableFramework.Context;
+using Sam.FileTableFramework.Extentions;
 
 namespace Sam.Persistence
 {
     public class DatabaseContext : FileTableDBContext
     {
+        public DatabaseContext(DatabaseOptions options) : base(options)
+        {
+        }
+
         public FtDbSet Table1 { get; set; }
         public FtDbSet Table2 { get; set; }
         public FtDbSet Table3 { get; set; }
